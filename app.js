4ed553533fb1ee704445c288e39799c2f754d08f
isameer3056 <isameer3056@gmail.com>
2024-11-28 22:42:06 +0530

    Minor changes with final update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! This is simple Node.js App !! \n');
+  res.send('Hello, World! This is Isameer3056, here is my simple Node.js App !! \n');
 });
 
 app.listen(port, () => {
